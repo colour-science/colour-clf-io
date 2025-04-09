c0f2b5cef97ccb004389439e698bb4cb26ef9ea5
Michael Mauderer <michael@mauderer.me>
2025-04-09 13:22:07 +0100

    Clean up tests file.

--- colour_clf_io/tests/processing/test_asc_cdl.py
+++ colour_clf_io/tests/processing/test_asc_cdl.py
@@ -1,7 +1,4 @@
-# !/usr/bin/env python
 """Define the unit tests for the :mod:`colour.io.clf` module."""
-
-import unittest
 
 __author__ = "Colour Developers"
 __copyright__ = "Copyright 2013 Colour Developers"
@@ -121,7 +118,3 @@ class TestASC_CDL:
         </ASC_CDL>
         """
         assert_snippet_consistency(example)
-
-
-if __name__ == "__main__":
-    unittest.main()
